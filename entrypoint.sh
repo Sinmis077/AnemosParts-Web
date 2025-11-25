19ebe0af69ef5305423bc762ef7cdfbf78147231
Ioannis Panagi <llelga8@gmail.com>
2025-11-25 04:17:43 +0100

    missing .

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 envsubst < /app/dist/config.template.js > ./dist/config.js
 
 echo "Generated config.js:"
-cat /dist/config.js
+cat ./dist/config.js
 
 exec serve -s dist -l tcp://0.0.0.0:3000
